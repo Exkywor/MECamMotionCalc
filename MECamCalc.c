a9f8c7ffd062578910e11c3cadc9bf3b9c1a78ae
Exkywor <fernandoquant.dev@gmail.com>
2021-05-24 21:18:36 -0600

    Add relativize function. Add .vscode files

--- MECamCalc.c
+++ MECamCalc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void *promptValues(float *arr, char *name);
+float relativize(float lenghts[], float startTime, float endTime, float point[]);
 
 int main() {
 	int span;
@@ -43,16 +44,16 @@ int main() {
 	float rotE[3];
 	// Prompt for values
 	promptValues(posS, "starting position");
-	promptValues(rotS, "starting rotation");
-	promptValues(posE, "ending position");
-	promptValues(rotE, "ending rotation");
+	// promptValues(rotS, "starting rotation");
+	// promptValues(posE, "ending position");
+	// promptValues(rotE, "ending rotation");
 	
 	// Prompt for how many points the user wants to find
 	int nToFind;
 	printf("How many points do you want to find? ");
 	scanf("%d", &nToFind);
 	float pointsToFind[nToFind][2];
-	// NEED TO VALIDATE THAT THEY ARE NOT OUTISDE INTERPDATA BOUNDS
+	// NEED TO VALIDATE THAT THEY ARE NOT OUTSIDE INTERPDATA BOUNDS
 	for (int i = 0; i < nToFind; i++) {
 		int s;
 		float t;
@@ -64,14 +65,40 @@ int main() {
 		pointsToFind[i][1] = t;
 	}
 	
+	// [Point1>[Pos>[x,y,z],Rot>[x,y,z]]]
+	float resPoints[nToFind][2][3];
+	for (int i = 0; i < nToFind; i++) {
+		printf("%f\n", relativize(lengths, startTime, endTime, pointsToFind[i]));
+	}
 	
 	return 0;
 }
 
 // DOES NOT HAVE VALIDATION
-// Request Starting position and rotation
+// Request user for positional and rotational values
 void *promptValues(float *arr, char *name) {
 	printf("Input %s as comma separated values (x,y,z): ", name);
 	scanf("%f,%f,%f", &arr[0], &arr[1], &arr[2]);
 	printf("%f %f %f\n", arr[0], arr[1], arr[2]);
+}
+
+// The lengths of the InterpDatas, the startTime and endTime in InterpDatas 0 and n,
+// and the point to convert to relative time [interp, time in interp]
+float relativize(float lengths[], float startTime, float endTime, float point[]) {
+	float realTime;
+	for (int i = 0; i <= point[0]; i++) {
+		if (i == 0) {
+			if ((int)point[0] == 0) { // Target interp is the first interp
+				 realTime = ((lengths[0] - startTime) - (lengths[0] - point[1]));
+				 break;
+			} else {
+				realTime = lengths[0] - startTime;
+			}
+		} else if (i == point[0]) { // Reached target Interp. We automatically stop here, but the math is different
+			realTime += point[1];
+		} else { // Add intermidiate interp length
+			realTime += lengths[i];
+		}
+	}
+	return realTime;
 }
